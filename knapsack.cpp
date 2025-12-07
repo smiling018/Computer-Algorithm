419d5c167d58fc3b940795425a54cf188d47c99a
Jannatul Ferdous <inshirajahanirha@gmail.com>
2025-12-07 10:41:59 +0600

    Add files via upload

--- knapsack.cpp
+++ knapsack.cpp
@@ -1,22 +1,23 @@
 #include <iostream>
-#include <vector>
 #include <algorithm>
 using namespace std;
-
-int knapsack_dp(vector<int> &values, vector<int> &weights, int W)
+int knapsack_dp(int values[], int weights[], int n, int W)
 {
-    int n = values.size();
-
-    vector<vector<int>> dp(n + 1, vector<int>(W + 1, 0));
-
+    int dp[n + 1][W + 1];
+    for (int i = 0; i <= n; i++)
+    {
+        for (int j = 0; j <= W; j++)
+        {
+            dp[i][j] = 0;
+        }
+    }
     for (int i = 1; i <= n; i++)
     {
         for (int j = 1; j <= W; j++)
         {
             if (weights[i - 1] <= j)
             {
-                dp[i][j] = max(dp[i - 1][j],
-                               values[i - 1] + dp[i - 1][j - weights[i - 1]]);
+                dp[i][j] = max(dp[i - 1][j], values[i - 1] + dp[i - 1][j - weights[i - 1]]);
             }
             else
             {
@@ -24,33 +25,27 @@ int knapsack_dp(vector<int> &values, vector<int> &weights, int W)
             }
         }
     }
-
     return dp[n][W];
 }
-
 int main()
 {
     int n;
     cout << "Number of items: ";
     cin >> n;
-
-    vector<int> weights(n), values(n);
-
+    int W;
+    cout << "Enter Capacity : ";
+    cin >> W;
+    int weights[n], values[n];
     cout << "Enter weights of each item:\n";
     for (int i = 0; i < n; i++)
+    {
         cin >> weights[i];
-
-    cout << "Enter profits/values of each item:\n";
-    for (int i = 0; i < n; i++)
+    }
+    cout << "Enter profits of each item:\n";
+    for (int i = 0; i < n; i++){
         cin >> values[i];
-
-    int W;
-    cout << "Enter Knapsack Capacity W: ";
-    cin >> W;
-
-    int maxProfit = knapsack_dp(values, weights, W);
-
+    }
+    int maxProfit = knapsack_dp(values, weights, n, W);
     cout << "Maximum Profit = " << maxProfit << endl;
-
     return 0;
 }
